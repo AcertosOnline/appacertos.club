5204b1856fe19bd511d3906e02cb29eaa111618e
AcertosOnline <acertosonlinejogodobicho@gmail.com>
2025-05-26 15:54:55 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
-// Execute when the page loads
 document.addEventListener('DOMContentLoaded', function() {
-  // Get the current domain (e.g., https://example.com)
   const domain = window.location.origin;
-  // Construct the new URL
   const newUrl = `${domain}/pr/fC7hpda9`;
-  // Update the URL in the browser's navigation bar without reloading
   window.history.replaceState(null, null, newUrl);
 });
+
+
