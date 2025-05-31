488cccec90b5eaa9d4a548754e0c8b2126f89d3b
AcertosOnline <acertosonlinejogodobicho@gmail.com>
2025-05-30 21:36:27 -0300

    Update app.js

--- app.js
+++ app.js
@@ -59,10 +59,3 @@ const pwaBanner = `
       });
   }
 });
-
-
-    // Redireciona para o site externo apenas em modo standalone
-    if (window.matchMedia('(display-mode: standalone)').matches) {
-      window.location.href = 'https://app.acertos.club/pr/fC7hpda9';
-    }
-
