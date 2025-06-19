8ecc9d6a8245870b74c351d4e0219ba7c13ecf10
AcertosOnline <acertosonlinejogodobicho@gmail.com>
2025-06-18 22:29:25 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -27,16 +27,7 @@ function setupSectionAnimations() {
     });
 }
 
-/* Footer Section Toggling */
-function showSection(sectionId) {
-    document.querySelectorAll('.content-section').forEach(section => {
-        section.classList.add('hidden');
-    });
-    document.getElementById(sectionId).classList.remove('hidden');
-}
-
 /* Initialize on Page Load */
 document.addEventListener('DOMContentLoaded', () => {
-    updateLinks();
     setupSectionAnimations();
 });
